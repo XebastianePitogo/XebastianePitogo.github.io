f71e2c737abe9e0e47aa8adbc9fd614747f074df
XebastianePitogo <148425795+XebastianePitogo@users.noreply.github.com>
2023-11-02 14:16:56 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
+window.onload = function() {
+  setTimeout(function() {
+    document.getElementById('preloader').style.display = 'none';
+  }, 1000);
+};
+
 window.addEventListener('resize', function(event) {
     var width = window.innerWidth;
   
     if (width < 600) {
-      // Code for screens smaller than 600px
     } else {
-      // Code for screens larger than 600px
     }
   });
-
-window.addEventListener('load', function() {
-    document.getElementById('preloader').style.display = 'none';
-});
 
 const navigation = document.querySelector(".primary-navigation");
 
@@ -20,3 +20,4 @@ document.documentElement.style.setProperty(
   "--scroll-padding",
   navigationHeight + "px"
 );
+
